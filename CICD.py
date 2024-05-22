be81932f524fde82b1d283cfeba45a050858ce0c
Chenwingu <ngu@ctech247.onmicrosoft.com>
2024-05-22 11:26:54 -0400

    Update CICD.py

--- CICD.py
+++ CICD.py
@@ -1,22 +1,20 @@
-from diagrams import Diagram, Cluster
-from diagrams.custom import Custom
+from diagrams import Diagram, Cluster, Custom
 from diagrams.onprem.ci import Jenkins
 from diagrams.onprem.vcs import Github
 from diagrams.onprem.container import Docker
 from diagrams.programming.language import Java
-from diagrams.generic.storage import Storage
-from diagrams.onprem.compute import Server
 
-with Diagram("CI/CD Pipeline", show=False, graph_attr={"ranksep": "0.7", "nodesep": "0.4", "splines": "polyline", "ratio": "0.15"}):
+
+with Diagram("CI/CD Pipeline", show=False, graph_attr={"ranksep": "0.7", "nodesep": "0.4"}):
     
-    with Cluster("New Feature - Development", graph_attr={"fontsize": "20"}):
+    with Cluster("New Feature - Development", graph_attr={"fontsize": "10"}):
          ticket = Custom("Ticket raised/assigned", "/path/to/jira.png")
          developer = Java("Write/test code")
          github = Github("Developer pushes code")
  
          ticket >> developer >> github 
 
-    with Cluster("Continuous Integration", graph_attr={"fontsize": "20"}):
+    with Cluster("Continuous Integration", graph_attr={"fontsize": "10"}):
         jenkins = Jenkins("Jenkins")
         maven_test = Custom("Compile code/Unit test", "/path/to/maven.png")
         sonarqube = Custom("Code Quality Check", "/path/to/sonarqube.png")
@@ -27,7 +25,7 @@ with Diagram("CI/CD Pipeline", show=False, graph_attr={"ranksep": "0.7", "nodese
 
     github >> jenkins
 
-    with Cluster("Artifact - Docker Image", graph_attr={"fontsize": "20"}):
+    with Cluster("Artifact - Docker Image", graph_attr={"fontsize": "10"}):
          nexus = Custom("Push Artifact", "/path/to/nexus.png")
          docker_build = Docker("Docker build and Tag")
          trivy = Custom("Docker Image Scan", "/path/to/trivy.png")
@@ -36,7 +34,7 @@ with Diagram("CI/CD Pipeline", show=False, graph_attr={"ranksep": "0.7", "nodese
 
     maven_build >> nexus
 
-    with Cluster("Continuous Delivery", graph_attr={"fontsize": "20"}):
+    with Cluster("Continuous Deployment", graph_attr={"fontsize": "10"}):
          docker_push = Docker("Docker Push")
          kubernetes = Custom("Deploy to Kubernetes",  "/path/to/kubernetes.png")
 
